0fc10543e1b4fc78d1439b9abf53a66a159c3fb6
Richard Nelson <unixabg@gmail.com>
2014-09-08 13:24:47 -0500

    Minor format adjustment in scripts/add_user.php

--- scripts/add_user.php
+++ scripts/add_user.php
@@ -2,21 +2,21 @@
 require_once('../../../../php_scripts/mysql_connect.php');
 if (isset($_POST['first_name'], $_POST['last_name'], $_POST['username'], $_POST['email'], $_POST['password'], $_POST['password_confirm']) && $_POST['password'] == $_POST['password_confirm']) {
 	if (!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_confirm'])) {
-	$email = $_POST['email'];
-	$first_name = $_POST['first_name'];
-	$last_name = $_POST['last_name'];
-	$username = $_POST['username'];
-	$password = md5($_POST['password']);
+		$email = $_POST['email'];
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$username = $_POST['username'];
+		$password = md5($_POST['password']);
 
-	$query = "INSERT INTO users (email, password, firstname, lastname, username) VALUES (\"$email\", \"$password\", \"$first_name\", \"$last_name\", \"$username\")";
-	$result = $db->query($query);
-	if ($result) {
-		session_start();
-		header('Location: ./index.php');
-		$_SESSION['username'] = $username;
-	}else {
-		echo "Error adding user.";
-	}
+		$query = "INSERT INTO users (email, password, firstname, lastname, username) VALUES (\"$email\", \"$password\", \"$first_name\", \"$last_name\", \"$username\")";
+		$result = $db->query($query);
+		if ($result) {
+			session_start();
+				header('Location: ./index.php');
+			$_SESSION['username'] = $username;
+		}else {
+			echo "Error adding user.";
+		}
 	} else {
 		echo "All forms were not submited.";
 	}

049b0ac5ded4b158342bd8bffc2b803aa9e0b47e
johnburd <johnburd19@gmail.com>
2014-09-25 20:41:06 +0000

    Dropped videos table in mysql database.

--- scripts/tsytdl.sql
+++ scripts/tsytdl.sql
@@ -15,16 +15,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/*
-	To source in table
-	-- Login mysql using: mysql -u <username> -p
-	-- Then source in using: source tsytdl.sql
-*/
-
 --
 -- Table structure for table `users`
 --
 
+/* CREATE DATABASE tsyldl IF DOES NOT EXISTS */
 DROP DATABASE IF EXISTS `tsytdl`;
 CREATE DATABASE `tsytdl`;
 USE `tsytdl`;
@@ -40,33 +35,6 @@ CREATE TABLE `users` (
   `firstname` char(32) DEFAULT NULL,
   `lastname` char(32) DEFAULT NULL,
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
---
--- Table structure for table `videos`
---
-
-DROP TABLE IF EXISTS `videos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `videos` (
-  `videoid` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `userid` int(10) DEFAULT NULL,
-  `title` varchar(101) DEFAULT NULL,
-  `link` varchar(124) DEFAULT NULL,
-  `url` char(156) DEFAULT NULL,
-  PRIMARY KEY (`videoid`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-09-23 13:33:18
+-- Dump completed on 2014-09-25 13:23:46

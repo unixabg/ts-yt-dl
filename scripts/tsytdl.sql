80d8a26f0fa35ea57c1d8e7f15e9c15903d2d894
johnburd <johnburd19@gmail.com>
2014-10-14 20:34:57 -0500

    Updated tsytdl.sql to create user on sync.

--- scripts/tsytdl.sql
+++ scripts/tsytdl.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+/* CREATE user for database */
+CREATE USER `tsytdl`@`localhost` IDENTIFIED BY 'mypass';
+GRANT DELETE, INSERT, SELECT, UPDATE ON tsytdl.* TO `tsytdl`;
 --
 -- Table structure for table `users`
 --

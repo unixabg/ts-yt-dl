724ab72fc1c360e72ea3bf72a23e3a2919d44d8c
johnburd <jburd@silverburd.com>
2014-10-22 13:47:59 -0500

    Added file for last commit

---
+++ scripts/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TimeShifter-YouTube-DownLoader</title>
+	<link rel="stylesheet" type="text/css" href="./style/main.css">
+	<link rel="stylesheet" href="./style/login.css">
+</head>
+<div id="banner">
+	<h1>TS-YT-DL</h1>
+</div>
+<body>
+	<h4>You account must be approved.</h4>
+</body>
+</html>

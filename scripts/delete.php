211ef46d41a6e99a14c054a92711acec4a90f630
johnburd <jburd@silverburd.com>
2014-10-29 21:02:35 -0500

    Added delete function to delete videos.

---
+++ scripts/delete.php
@@ -0,0 +1,28 @@
+<?php
+require_once("./functions.php");
+require('../../ts-yt-dl-defaults/ts-yt-dl');
+login_check();
+// Check if ts_id is set
+if (isset($_GET['ts_id'])) {
+	$ts_id = $_GET['ts_id'];
+	$media_path = "$data_path/$userid/videos/$ts_id/";
+	// Check if video exists
+	if (is_dir("$media_path")) {
+		// First we have to delete all files in the directory
+		$media_dir = scandir("$media_path");
+		$media_dir_count = count($media_dir);
+		for ($x = 0; $x < $media_dir_count; $x++) {
+			if ($media_dir[$x] != "." && $media_dir[$x] != "..") {
+				unlink($media_path."/".$media_dir[$x]);
+				echo $media_dir[$x];
+			}
+		}
+		rmdir($media_path);
+		header("Location: ./videos.php");
+	} else {
+		echo "Could not find file on server.";
+	}
+} else {
+	echo "No data was entered.";
+}
+?>
--- scripts/videos.php
+++ scripts/videos.php
@@ -15,6 +15,7 @@ echo "<body>
 					<th class=\"medium_cell\"><h4>TS Ref. Number</h4></th>
 					<th class=\"large_cell\"><h4>Title</h4></th>
 					<th class=\"medium_cell\"><h4>File Size</h4></th>
+					<th class=\"small_th\"></th>
 				</tr>";
 	for ($t = 0; $t < $timestamp_count; $t++) {
 		// Skip over the "." and ".." files
@@ -33,6 +34,7 @@ echo "<body>
 							echo "<td class=\"medium_cell\">".$timestamp[$t]."</td>";
 							echo "<td class=\"large_cell\"><h4 class=\"media_title\">".$video[$v]."</h4></td>";
 							echo "<td class=\"medium_cell\">". number_format(filesize("$data_path/$userid/videos/".$timestamp[$t]."/".$video[$v]) / 1024, 2) ." KB</td>";
+							echo "<td class=\"small_cell\"><a onclick=\"return confirm('Are you sure you to delete this?')\" href=\"./delete.php?ts_id=".$timestamp[$t]."\" class=\"saltire\" > Delete</a></td>";
 						echo "</tr>";
 					}
 				}
@@ -42,5 +44,4 @@ echo "<body>
 echo "</table>";
 echo "		<input class=\"download_submit\" type=\"submit\" value=\"Time Shift to DVD (FIXME)\">
 	</form>";
-
 ?>

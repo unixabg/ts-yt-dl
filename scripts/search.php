413355c0657e3d5daa10d68e3fa9a5322d0cee76
johnburd <jburd@silverburd.com>
2014-12-01 20:23:14 -0600

    Updated search.php to search through audios.

--- scripts/search.php
+++ scripts/search.php
@@ -8,14 +8,15 @@ echo "<link rel=\"stylesheet\" href=\"./style/content_list.css\">";
 	<?php
 	if (isset($_GET['search']) && !empty($_GET['search'])) {
 		$search = $_GET['search'];
+		$count = 0;
 		echo "<p>Search results for <i>\"$search\"</i></p>";
 		echo "<table id=\"media_table\">
 				<tr>
 					<th class=\"medium_cell\"><h4>TS Ref. Number</h4></th>
 					<th class=\"large_cell\"><h4>Title</h4></th>
 					<th class=\"medium_cell\"><h4>File Size</h4></th>
+					<th class=\"medium_cell\"><h4>File Type</h4></th>
 				</tr>";
-		$count = 0;
 		$dir = scandir("$data_path/$userid/videos/");
 		foreach ($dir as $timestamp) {
 			if ($timestamp != "." && $timestamp != "..") {
@@ -30,6 +31,29 @@ echo "<link rel=\"stylesheet\" href=\"./style/content_list.css\">";
 									echo "<td class=\"medium_cell\">".$timestamp."</td>";
 									echo "<td class=\"large_cell\"><a href=\"./play_media.php?timestamp=".$timestamp."&media=".$video."&page=videos\"><h4 class=\"media_title\">&#9658 ".$video."</h4></a></td>";
 									echo "<td class=\"medium_cell\">". number_format(filesize("$data_path/$userid/videos/".$timestamp."/".$video) / 1024, 2) ." KB</td>";
+									echo "<td>Video</td>";
+								echo "</tr>";
+							}
+						}
+					}
+				}
+			}
+		}
+		$dir = scandir("$data_path/$userid/audios/");
+		foreach ($dir as $timestamp) {
+			if ($timestamp != "." && $timestamp != "..") {
+				$file = scandir("$data_path/$userid/audios/$timestamp");
+				foreach ($file as $audio) {
+					if ($audio != "." && $audio != "..") {
+						$ext = pathinfo($audio, PATHINFO_EXTENSION);
+						if ($ext == 'mp3') {
+							if (stristr($audio, $search)) {
+								$count++;
+								echo "<tr>";
+									echo "<td class=\"medium_cell\">".$timestamp."</td>";
+									echo "<td class=\"large_cell\"><a href=\"./play_media.php?timestamp=".$timestamp."&media=".$audio."&page=audios\"><h4 class=\"media_title\">&#9658 ".$audio."</h4></a></td>";
+									echo "<td class=\"medium_cell\">". number_format(filesize("$data_path/$userid/audios/".$timestamp."/".$audio) / 1024, 2) ." KB</td>";
+									echo "<td>Audio</td>";
 								echo "</tr>";
 							}
 						}

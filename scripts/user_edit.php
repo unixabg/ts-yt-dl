6c47a0c51f9ec2c7148e346d566218678d773940
johnburd <jburd@silverburd.com>
2014-12-04 13:29:17 -0600

    Inital commit for email.

--- scripts/user_edit.php
+++ scripts/user_edit.php
@@ -13,10 +13,24 @@ if (isset($_POST['action']) && isset($_POST['userid'])) {
 	$cust_id = $_POST['userid'];
 	$status = $_POST['status'];
 	$username = $_POST['username'];
+	if ($status == 0) {
+		$message = "Your account is pending to be approved.";
+	} elseif ($status == 1) {
+		$message = "Your account was approved.";
+	} elseif ($status == 5) {
+		$message = "Your account was blocked. Please contact you administor for more information.";
+	} elseif ($status == 10) {
+		$message = "Your account was ugraded to admin.";
+	}
 	if ($action == "Save Changes") {
 		$query = "UPDATE users SET authorized = $status, username = \"$username\" WHERE userid = $cust_id";
 		$update_user = $db->query($query);
+		$email_query = "SELECT * FROM users WHERE userid = $userid";
+		$email_rs = $db->query($email_query);
+		$user_array = $email_rs->fetch_assoc();
+		$email = $user_array['email'];
 		if ($update_user) {
+			mail($email, "TS-YT-DL", $message);
 			file_put_contents("$data_path/$cust_id/user.log","[$date]\tUser status changed to \"$status\".\n", FILE_APPEND);
 			file_put_contents("$admin_log", "[$date] Admin $userid: Changed user \"$cust_id\" status to \"$status\".\n", FILE_APPEND);
 			header("Location: ./admin.php");

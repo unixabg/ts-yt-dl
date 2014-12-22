69a392b7c14d0fca2816f427b12148ab01b59b2a
Richard Nelson <unixabg@gmail.com>
2014-12-22 14:20:27 -0600

    Inital modification for header update and css harmony on homepage.

--- homepage/header.php
+++ homepage/header.php
@@ -1,8 +1,12 @@
+<!DOCTYPE html>
 <html>
 <head>
 	<title>TimeShifter-YouTube-DownLoader</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<div id="nav">
+		<link rel="stylesheet" type="text/css" href="./style/main.css">
+		</head>
+		<body>
+		<div id="banner">
+			<h1 class="logo"><a href="./home.php">TS-YT-DL</a></h1>
 		<ul class="navul">
 			<li class="navli"><a class="nava" href="about.php">About</a></li>
 			<li class="navli"><a class="nava" href="vcs.php">VCS</a></li>

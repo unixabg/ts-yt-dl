212313b5acb856ef55f5e6bd3a7a29bdf1128765
johnburd <jburd@silverburd.com>
2015-01-30 21:15:37 -0600

    Added redirection for logged in users to homepage and redirect first time users to setup page in scripts/index.php

--- scripts/index.php
+++ scripts/index.php
@@ -11,6 +11,17 @@
 <body>
 	<div id="content">
 	<?php
+	session_start();
+	if (isset($_SESSION['username']) && !empty($_SESSION['username'])) {
+		header("Location: ./home.php");
+	}
+	require_once('./mysql_connect.php');
+	$scan_users = "SELECT username as total FROM users";
+	$result = $db->query($scan_users);
+	if ($result->num_rows == 0) {
+		header("Location: ./setup.php");
+		exit();
+	}
 	require('../../ts-yt-dl-defaults/ts-yt-dl');
 	echo "<h2 class=\"h2_title\">Sign in</h2>";
 	if (isset($_GET['error'])) {

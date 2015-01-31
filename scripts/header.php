ac593d3a53335da75a3e10611ccb64aeafd70266
johnburd <jburd@silverburd.com>
2015-01-30 21:13:59 -0600

    Cleaned up indentation on scripts/header.php.

--- scripts/header.php
+++ scripts/header.php
@@ -12,29 +12,29 @@
 	require("./mysql_connect.php");
 	login_check();
 	if (isset($userid)) {
-	$query_username = "SELECT firstname, lastname FROM users WHERE userid = $userid";
-	$result = $db->query($query_username);
-	$user = $result->fetch_assoc();
-	$name = $user['firstname']." ".$user['lastname'];
-	echo "<form class=\"search\" action=\"search.php\" method=\"GET\">
-			<input class=\"search_input\" type=\"text\" name=\"search\" placeholder=\"Search\">
-			<input class=\"search_submit\" type=\"submit\" value=\"Search\">
-			</form>
-			<p class=\"full_name\">Hello, $name</p>
-			<a class=\"logout\" href=\"./logout.php\">Logout</a>
-		</div>
-		<nav>
-				<ul>
-					<li><a href=\"./home.php\">Home</a></li>
-					<li><a href=\"./audios.php\">Audios</a></li>
-					<li><a href=\"./videos.php\">Videos</a></li>
-					<li><a href=\"./account.php\">My Account</a></li>";
-					if ($_SESSION['authorized'] == 10) {
-						echo "<li><a href=\"./admin.php\">Admin</a></li>";
-					}
-					echo "<li><a href=\"./support.php\">Support</a></li>
-				</ul>
-			</nav>";
+		$query_username = "SELECT firstname, lastname FROM users WHERE userid = $userid";
+		$result = $db->query($query_username);
+		$user = $result->fetch_assoc();
+		$name = $user['firstname']." ".$user['lastname'];
+		echo "<form class=\"search\" action=\"search.php\" method=\"GET\">
+				<input class=\"search_input\" type=\"text\" name=\"search\" placeholder=\"Search\">
+				<input class=\"search_submit\" type=\"submit\" value=\"Search\">
+				</form>
+				<p class=\"full_name\">Hello, $name</p>
+				<a class=\"logout\" href=\"./logout.php\">Logout</a>
+			</div>
+			<nav>
+					<ul>
+						<li><a href=\"./home.php\">Home</a></li>
+						<li><a href=\"./audios.php\">Audios</a></li>
+						<li><a href=\"./videos.php\">Videos</a></li>
+						<li><a href=\"./account.php\">My Account</a></li>";
+						if ($_SESSION['authorized'] == 10) {
+							echo "<li><a href=\"./admin.php\">Admin</a></li>";
+						}
+						echo "<li><a href=\"./support.php\">Support</a></li>
+					</ul>
+				</nav>";
 	} else {
 		echo "</div>";
 	}

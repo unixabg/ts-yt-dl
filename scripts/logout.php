404f3a9a1f62f675185ec29bfee1dd0f4324c328
johnburd <jburd@silverburd.com>
2015-01-30 21:16:38 -0600

    Destroy session variables on logout in scripts/logout.php

--- scripts/logout.php
+++ scripts/logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+session_destroy();
 $date = date("Y-m-d H:i:s");
 file_put_contents("/srv/ts-yt-dl/".$_SESSION['userid']."/user.log", "[$date]\tUser logged out of account.\n", FILE_APPEND);
 header('Location: ./index.php');

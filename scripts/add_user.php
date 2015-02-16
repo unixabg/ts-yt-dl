28fe4fab10d3de4a5ec4c170c6bc39c440ea6b8a
Richard Nelson <unixabg@gmail.com>
2015-02-16 10:05:01 -0600

    Create users audios and videos folder when we create userid folder in scripts/add_user.php.

--- scripts/add_user.php
+++ scripts/add_user.php
@@ -54,6 +54,8 @@ if (isset($_POST['first_name'], $_POST['last_name'], $_POST['username'], $_POST[
 			$message = "[$date]\tUser requested authorization for account.\n";
 			if ( !is_dir("$data_path/".$user_id['userid']) ) {
 				mkdir("$data_path/".$user_id['userid']."/", 0755, true);
+				mkdir("$data_path/".$user_id['userid']."/audios/", 0755, true);
+				mkdir("$data_path/".$user_id['userid']."/videos/", 0755, true);
 			}
 			file_put_contents($data_path."/".$user_id['userid']."/user.log", $message);
 			// If this is the setup account or authorization is turned off send the user to the home page

903a02a0f95f8c1632d7d00480c2f15eab51847e
Richard Nelson <unixabg@gmail.com>
2016-10-12 14:24:46 -0500

    Add support for video seeking on playback on scripts/play_media.php.

--- scripts/play_media.php
+++ scripts/play_media.php
@@ -9,7 +9,7 @@ if ($_GET['page'] == "videos") {
 	echo "<div class=\"video_player\">
 					<h1 class=\"header\">$media</h1>
 					<video width=\"520\" height=\"340\" controls>
-						<source src=\"./local_download.php?timestamp=$timestamp&media=$media&page=$page&public=$public\"/>
+						<source src=\"./local_download.php?timestamp=$timestamp&media=$media&page=$page&public=$public\" type=\"video/mp4\"/>
 					</video>
 					<a class=\"download_link\" href=\"./local_download.php?timestamp=$timestamp&media=$media&page=$page&public=$public\">Download</a>";
 } else {

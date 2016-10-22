63d1a6c021d9ef29a239d600565f2a08a000deae
Richard Nelson <unixabg@gmail.com>
2016-10-22 08:54:26 -0500

    Allow admin to upload to public media in scripts/home.php.

--- scripts/home.php
+++ scripts/home.php
@@ -14,7 +14,7 @@ echo "<div id=\"content\">";
 			<p class=\"radio_input\">
 				Video: <input lable=\"High\" type=\"radio\" name=\"dtype\" value=\"video\" checked>
 				&nbsp;&nbsp;Audio only: <input type=\"radio\" name=\"dtype\" value=\"audio\"><br>";
-				if ($_SESSION['authorized'] == 3) {
+				if ($_SESSION['authorized'] == 3 || $_SESSION['authorized'] == 10) {
 					echo "&nbsp;&nbsp; Public Video: <input type=\"radio\" name=\"dtype\" value=\"public_video\">
 					&nbsp;&nbsp; Public Audio: <input type=\"radio\" name=\"dtype\" value=\"public_audio\">";
 				}
